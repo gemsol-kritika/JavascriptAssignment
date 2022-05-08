8212ff88e4f6a7ebeb568f788ccd5540f84b4546
gemsol-kritika <kritika.sharma@geminisolutions.com>
2022-05-08 13:15:04 +0530

    done some modifications in the function validation

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -1,63 +1,65 @@
 
+var index=1;
+var user;
+var userEmail;
+var userPhone;
+var isNameValid;
+var isEmailValid;
+var isPhoneValid;
+function util(){
+isNameValid=true;
+isEmailValid=true;
+isPhoneValid=true;
+user = document.getElementById("user").value;
+userEmail = document.getElementById("useremail").value;
+userPhone = document.getElementById("userphone").value;
+validation();
+}
+
+
 function validation() {
-    var index=1;
-    var user = document.getElementById("user").value;
-    var userEmail = document.getElementById("useremail").value;
-    var userPhone = document.getElementById("userphone").value;
-  //   for (var i = 0; i < localStorage.length; i++) {
-  //     var key   = localStorage.key(i);
-  //     var value = localStorage.getItem(key);
-  
-  //     console.log(key + ": " + value);
-  // }
-function isNameOfUserValid(){
     if (user == "") {
+      isNameValid=false;
       document.getElementById("username").innerHTML =
         "PLEASE FILL THE VALUE FOR THE NAME FIELD";
       return false;
     }
     if (user.length <= 2 || user.length >= 25) {
+      isNameValid=false;
       document.getElementById("username").innerHTML =
         "PLEASE ENTER A VALID NAME";
       return false;
     }
     if (!isNaN(user)) {
+      isNameValid=false;
       document.getElementById("username").innerHTML = "DIGITS NOT ALLOWED";
       return false;
     }
-
-function checkUserName() {
 var user_name = document.getElementsByName("user").value;
 var pattern = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/); //unacceptable chars
 if (pattern.test(user_name)) {
+  isNameValid=false;
     document.getElementById("username").innerHTML = "SPECIAL CHARACTERS NOT ALLOWED";
       return false;
 }
-return true; 
-}
-return true;
-}
-function isEmailOfUserValid(){
     if (userEmail == "") {
+      isEmailValid=false;
       document.getElementById("emailuser").innerHTML =
         "PLEASE FILL THE VALUE FOR THE EMAIL FIELD";
       return false;
     }
-    return true;
-}
-function isPhoneNumberOfUserValid(){
     if (userPhone != "" && userPhone.length < 10) {
+      isPhoneValid=false;
       document.getElementById("numberuser").innerHTML =
         "THE VALUE FOR THE MOBILE NUMBER IS INCORRECT";
       return false;
     }
-    return true;
-}
-  if (isNameOfUserValid && isEmailOfUserValid && isPhoneNumberOfUserValid) {
+  alert(user+" "+userPhone+" "+userEmail);
+ 
+  if (isNameValid && isEmailValid && isPhoneValid) {
     localStorage.setItem(index + "name",user);
     localStorage.setItem(index + "email",userEmail);
     localStorage.setItem(index+ "mobileno",userPhone);
-  
     showTable()
   }
 }
@@ -76,11 +78,11 @@ function showTable() {
     tHead.append(head);
     }
     row.innerHTML = `<tr>
-    <th scope="row">${serialNo+"."}</th>
+    <th scope="row">${index+"."}</th>
     <td>${localStorage.getItem(index + "name")}</td>
     <td>${localStorage.getItem(index + "email")}</td>
     <td>${localStorage.getItem(index + "mobileno")===null?"":localStorage.getItem(index + "mobileno")}</td>
   </tr>`;
     table.append(row);
     index++;
-}
+  }
